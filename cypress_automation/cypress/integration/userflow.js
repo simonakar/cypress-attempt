5d4ac2248b14f6d7b2b8dc3df82299cdfeda7fc3
simonakar <simonayak@gmail.com>
2020-01-31 11:23:43 +0200

    added missing quotes

--- cypress_automation/cypress/integration/userflow.js
+++ cypress_automation/cypress/integration/userflow.js
@@ -23,12 +23,12 @@ describe('userflow', function () {
     cy.visit(builder)
     cy.get('[data-qa="homepage-btn-startforfree"]').should('be.visible')
     cy.get('[data-qa="homepage-btn-startforfree"]').click()
-    cy.get('[data-qa=choosetemplate-btn-startbuilding]').first().should('be.visible')
+    cy.get('[data-qa="choosetemplate-btn-startbuilding"]').first().should('be.visible')
     cy.get('[data-qa="template-hover-state"]').first().invoke('show')
-    cy.get('button[data-qa=choosetemplate-btn-startbuilding]').first().invoke('show').click({ force: true }) //can't find how to un-hover button, 
+    cy.get('[data-qa="choosetemplate-btn-startbuilding"]').first().invoke('show').click({ force: true }) 
     cy.get('#app > div > div > div.app-main__column-main.app-main__column-main--sidebar-open > div > div > section > section:nth-child(2) > div.block-grid > div.block-grid-item.block-grid__item.is-current.is-movable > div > h1').should('be.visible')
    // cy.get('#block-grid-item block-grid__item is-current is-movable').type('{selectall}').type('updated title') // need to investigate how to select and edit text.
-   cy.get('[data-qa=builder-btn-publishsite]').click()
+   cy.get('[data-qa="builder-btn-publishsite"]').click()
    cy.get ('#email').should('be.visible')
    cy.get ('#email').type(incoming_mailbox)
    cy.get('[data-qa="auth-submit-button"]').click()
