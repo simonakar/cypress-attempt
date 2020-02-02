1e99fba7a3e5b5ba7370e67e575751211a155fac
simonakar <simonayak@gmail.com>
2020-02-02 21:41:15 +0200

    added version2, removed short tests

--- cypress_automation/cypress/integration/userflow.js
+++ cypress_automation/cypress/integration/userflow.js
@@ -5,21 +5,7 @@ describe('userflow', function () {
     const password = `test+${email}`;
     
        
-    it('should open the main page', function () {
-        cy.visit(builder)  //add to be executed before each test
-        cy.get('[data-qa="homepage-btn-startforfree"]').should('be.visible')
-
-    });
-
-    it('Open template list', function () {
-        cy.visit(builder)
-        cy.get('[data-qa="homepage-btn-startforfree"]').should('be.visible')
-        cy.get('[data-qa="homepage-btn-startforfree"]').click()
-
-    });
-
-
-  it('Publish edited template', function () {
+  it('Publish edited template-v1', function () {
     cy.visit(builder)
     cy.get('[data-qa="homepage-btn-startforfree"]').should('be.visible')
     cy.get('[data-qa="homepage-btn-startforfree"]').click()
@@ -39,7 +25,33 @@ describe('userflow', function () {
 
 });
 
+it('Publish edited template-v2', function () {
+    cy.visit(builder)
+    cy.viewport(1460, 937)
+    cy.get('[data-qa="homepage-btn-startforfree"]').should('be.visible')
+    cy.get('[data-qa="homepage-sectionfull-btn-explore"]').scrollIntoView()
+    //cy.get('[data-qa="cookiesmodal-message-gotit"]', { timeout: 100000 }).should('be.visible')
+    //cy.get('[data-qa="cookiesmodal-message-gotit"]').click()
+    cy.get('[data-qa="homepage-sectionfull-btn-explore"]').click()
+    cy.get('[data-qa="template-hover-state"]').first().scrollIntoView()
+    cy.get('[data-qa="choosetemplate-btn-startbuilding"]').first().scrollIntoView()
+    cy.scrollTo('top')
+    cy.get('[data-qa="choosetemplate-btn-startbuilding"]').first().invoke('show').click() 
+    cy.get('.h-1').should('be.visible')
+    cy.get('.h-1').dblclick().clear()  //clear and type works only for text area
+    cy.get('.h-1').type('updated title')  //  need to think how to select text and type
+    cy.get('[data-qa="builder-btn-publishsite"]').click()
+   cy.get ('#email').should('be.visible')
+   cy.get ('#email').type('email@email.com')
+   cy.get('[data-qa="auth-submit-button"]', { timeout: 100000 }).should('be.visible')
+   cy.get('[data-qa="auth-submit-button"]').click()
+   cy.get ('#name').type(password)
+   cy.get ('#password').type(password)
+   cy.get ('#name').type(password)
+   cy.get('[data-qa="auth-submit-button"]').click()
+  
 
+});
 
 
 
